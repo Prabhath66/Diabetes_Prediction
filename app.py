5a89938402dc95e737faf93c2c22f5574cf45dd2
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-06-10 07:58:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,13 @@ if st.button("Predict"):
 
     output_prediction=knn.predict(input_scaled) 
     st.write(output_prediction)
+    labeling_output={0:"Non-Diabetic", 1:"Pre-Diabetic", 2:"Diabetic"}
 
+    st.write(f"Prediction of the Given data results in {labeling_output.get(output_prediction, "Unknown")}")
     
+     # Message based on result
+    if result_label == "Pre-Diabetic":
+        st.warning("You are in the pre-diabetic range. It's advisable to consult a healthcare professional for further evaluation.")
+    elif result_label == "Diabetic":
+        st.error("You are classified as diabetic. Please seek medical advice for appropriate management.")
 
